3f63408891007bffd9e7b1dff8844d8b778083d0
Divine203 <divine00329@gmail.com>
2022-07-29 10:59:50 -0700

    Project-api

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 const fs = require("fs");
+const port = Process.env.PORT || 8081;
 
 app.get('/', (req, res) => {
    fs.readFile( __dirname + "/" + "projects.json", 'utf8', (err, data) => {
-      console.log( data );
+      // console.log( data );
       res.end( data );
    });
 })
 
-const server = app.listen(8081, function () {
-   const host = server.address().address
-   const port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+app.listen(port, () => {
+   // const host = server.address().address;
+   console.log("Server listening at port", port)
+});
